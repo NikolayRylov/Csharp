386a36dfdf8d4fa78089cdf3772bb1f9501a698d
Nikolay <lolkafkoo@gmail.com>
2019-02-22 15:59:17 +0300

    Cделал дз 1

---
+++ DZlesson1/DZlesson1/Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace DZlesson1
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static Image cat = Image.FromFile("Cat.jpg");
+
+
+
+        public static int Width { get; set; }               //Свойства       
+        public static int Height { get; set; }               //Ширина и высота поля
+        public static BaseObject[] _objs;
+
+        //public static void Load()
+        //{
+        //    Random rnd = new Random();
+        //    int rndSize;
+        //    _objs = new BaseObject[100];
+        //    for (int i = 0; i < _objs.Length; i++)
+        //    {
+        //        rndSize = rnd.Next(1, 20);
+        //        _objs[i] = new BaseObject(new Point(rnd.Next(0,1920), rnd.Next(0, 1920)), new Point(rnd.Next(-20, 20), 0), new Size(rndSize, rndSize));
+        //    }
+        //}
+        public static void Load()
+        {
+            Random rnd = new Random();
+            int rndSize;
+
+            _objs = new BaseObject[100];
+
+            for (int i = 0; i < _objs.Length / 2; i++)
+            {
+                rndSize = rnd.Next(1, 20);
+                _objs[i] = new BaseObject(new Point(rnd.Next(0, 1920), rnd.Next(0, 1080)), new Point(rnd.Next(-20, 20), 0), new Size(rndSize, rndSize));
+            }
+
+            for (int i = _objs.Length / 2; i < _objs.Length; i++)
+            {
+                rndSize = rnd.Next(1, 20);
+                _objs[i] = new Star(new Point(rnd.Next(1, 1920), rnd.Next(1, 1080)), new Point(i, 0), new Size(5,5));
+            }
+        }
+
+        static Game()
+        {
+
+        }
+
+        public static void Init (Form form)
+        {
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Load();
+
+            Timer timer = new Timer { Interval = 75 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+ 
+        }
+
+        public static void Draw()
+        {
+            Buffer.Graphics.DrawImage(cat, 0, 0, 1920, 1080);
+            Buffer.Graphics.FillEllipse(Brushes.Red, new Rectangle(70, 60, 150, 150));
+            Buffer.Graphics.FillEllipse(Brushes.YellowGreen, new Rectangle(1000, 580, 45, 45));
+
+
+
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }     
+        
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+    }
+}
---
+++ DZlesson1/DZlesson1/Star.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DZlesson1
+{
+    class Star : BaseObject
+    {       
+
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+
+        }        
+
+        public override void Draw()
+        {
+            Random r = new Random();
+            Pen br = new Pen(Color.FromArgb(r.Next(255), r.Next(255), r.Next(255)));
+
+            Game.Buffer.Graphics.DrawLine(br, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
+            Game.Buffer.Graphics.DrawLine(br, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+        }
+
+    }
+}
